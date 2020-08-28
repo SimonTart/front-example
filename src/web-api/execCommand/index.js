06d6f33e83abbdc1d4432b7611f2710cdd62dfb1
zengxuebing <zengxuebing.94@bytedance.com>
2020-08-28 14:26:05 +0800

    feat: execCommand example

--- src/web-api/execCommand/index.js
+++ src/web-api/execCommand/index.js
@@ -69,12 +69,142 @@ const commands = [
     text: '设置字体颜色',
     promptText: '请输入字体颜色',
   },
+  {
+    name: 'formatBlock',
+    text: '用块级元素包裹内容',
+    promptText: '请输入标签名',
+  },
+  {
+    name: 'forwardDelete',
+    text: '删除光标后的字符',
+  },
+  {
+    name: 'heading',
+    text: '设置 H1 ~ H6',
+  },
+  {
+    name: 'hiliteColor',
+    text: '修改背景色',
+    promptText: '请输入颜色值',
+  },
+  {
+    name: 'increaseFontSize',
+    text: '增大fontSize',
+  },
+  {
+    name: 'indent',
+    text: '缩进',
+  },
+  {
+    name: 'insertBrOnReturn',
+    text: '如何处理换行',
+  },
+  {
+    name: 'insertHorizontalRule',
+    text: '设置分割线',
+  },
+  {
+    name: 'insertHTML',
+    text: '设置 HTML',
+    promptText: '请输入HTML',
+  },
+  {
+    name: 'insertImage',
+    text: '插入图片',
+    promptText: '请输入图片链接',
+  },
+  {
+    name: 'insertOrderedList',
+    text: '插入有序列表',
+  },
+  {
+    name: 'insertUnorderedList',
+    text: '插入无序列表',
+  },
+  {
+    name: 'insertParagraph',
+    text: '插入段落',
+  },
+  {
+    name: 'insertText',
+    text: '插入文本',
+    promptText: '请输入插入的文本',
+  },
+  {
+    name: 'italic',
+    text: '斜体',
+  },
+  {
+    name: 'justifyCenter',
+    text: '居中对齐',
+  },
+  {
+    name: 'justifyFull',
+    text: '两端对齐',
+  },
+  {
+    name: 'justifyLeft',
+    text: '左对齐',
+  },
+  {
+    name: 'justifyRight',
+    text: '右边对齐',
+  },
+  {
+    name: 'outdent',
+    text: '取消缩进',
+  },
+  {
+    name: 'paste',
+    text: '粘贴',
+  },
+  {
+    name: 'redo',
+    text: '重做',
+  },
+  {
+    name: 'removeFormat',
+    text: '清楚格式',
+  },
+  {
+    name: 'selectAll',
+    text: '选中当前所有内容',
+  },
+  {
+    name: 'strikeThrough',
+    text: '删除线',
+  },
+  {
+    name: 'subscript',
+    text: '下标',
+  },
+  {
+    name: 'superscript',
+    text: '上标',
+  },
+  {
+    name: 'underline',
+    text: '下划线',
+  },
+  {
+    name: 'undo',
+    text: '撤销',
+  },
+  {
+    name: 'unlink',
+    text: '移除超链接',
+  },
+  {
+    name: 'styleWithCSS',
+    text: '使用 css 而不是 html 标签',
+    args: [true],
+  },
 ];
 
 function init() {
   const buttonList = commands.map((command) => {
     const button = document.createElement('button');
-    const { name, text, promptText } = command;
+    const { name, text, promptText, args } = command;
     let btnText = name + (text ? `(${text})` : '');
     if (!document.queryCommandSupported(name)) {
       button.setAttribute('disabled', true);
@@ -83,7 +213,13 @@ function init() {
     button.innerText = btnText;
 
     button.addEventListener('click', () => {
-      const success = document.execCommand(name, false, promptText ? window.prompt(promptText) : undefined);
+      let argArr = [];
+      if (promptText) {
+        argArr = [window.prompt(promptText)];
+      } else if (args) {
+        argArr = args;
+      }
+      const success = document.execCommand(name, false, ...argArr);
       console.log(`${name} command exec ${success ? 'success' : 'fail'}`);
     });
 
